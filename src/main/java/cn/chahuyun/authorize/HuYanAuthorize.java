c9b40910f7fa97d44ac1b55c9a99b71af42667af
Moyuyanli <RoxyGreerad@163.com>
2023-08-11 17:39:53 +0800

    -1.0.5 只为提交

--- src/main/java/cn/chahuyun/authorize/HuYanAuthorize.java
+++ src/main/java/cn/chahuyun/authorize/HuYanAuthorize.java
@@ -21,7 +21,7 @@ public final class HuYanAuthorize extends JavaPlugin {
     /**
      * 全局版本
      */
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.5";
     /**
      * 日志
      */
