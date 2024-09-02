24ed947b8b5ec87942a19cd774c69176f6fd37ee
Moyuyanli <RoxyGreerad@163.com>
2024-09-02 10:17:28 +0800

    feat:添加伴生方法，从伴生方法取到的用户保证唯一 docx:添加User方法文档

--- src/main/kotlin/cn/chahuyun/authorize/entity/User.kt
+++ src/main/kotlin/cn/chahuyun/authorize/entity/User.kt
@@ -2,6 +2,7 @@ package cn.chahuyun.authorize.entity
 
 import cn.chahuyun.authorize.constant.UserType
 import cn.chahuyun.authorize.constant.UserType.*
+import cn.chahuyun.hibernateplus.HibernateFactory
 import jakarta.persistence.*
 import net.mamoe.mirai.Bot
 import java.util.*
@@ -39,6 +40,81 @@ data class User(
 
     ) {
 
+    companion object {
+        /**
+         * 查询或创建一个 全局用户
+         *
+         * @see UserType.GLOBAL_USER
+         */
+        fun globalUser(userId: Long): User {
+            val map = mutableMapOf<String, Any?>(
+                Pair("type", GLOBAL_USER),
+                Pair("userId", userId)
+            )
+            HibernateFactory.selectOne(User::class.java, map)?.let {
+                return it
+            } ?: return User(type = GLOBAL_USER, userId = userId)
+        }
+
+        /**
+         * 查询或创建一个 群用户
+         *
+         * @see UserType.GROUP
+         */
+        fun group(groupId: Long): User {
+            val map = mutableMapOf<String, Any?>(
+                Pair("type", GROUP),
+                Pair("groupId", groupId)
+            )
+            HibernateFactory.selectOne(User::class.java, map)?.let {
+                return it
+            } ?: return User(type = GROUP, groupId = groupId)
+        }
+
+        /**
+         * 查询或创建一个 群管理用户
+         *
+         * @see UserType.GROUP_ADMIN
+         */
+        fun groupAdmin(groupId: Long): User {
+            val map = mutableMapOf<String, Any?>(
+                Pair("type", GROUP_ADMIN),
+                Pair("groupId", groupId)
+            )
+            HibernateFactory.selectOne(User::class.java, map)?.let {
+                return it
+            } ?: return User(type = GROUP_ADMIN, groupId = groupId)
+        }
+
+        /**
+         * 查询或创建一个 群成员用户
+         *
+         * @see UserType.GROUP_MEMBER
+         */
+        fun member(groupId: Long, userId: Long): User {
+            val map = mutableMapOf<String, Any?>(
+                Pair("type", GROUP_MEMBER),
+                Pair("groupId", groupId),
+                Pair("userId", userId)
+            )
+            HibernateFactory.selectOne(User::class.java, map)?.let {
+                return it
+            } ?: return User(type = GROUP_MEMBER, groupId = groupId, userId = userId)
+        }
+
+    }
+
+    /**
+     * 仅供于 Hibernate 查询使用。
+     *
+     * 需要创建 User 请使用伴生方法。
+     *
+     * @see User.globalUser
+     * @see User.group
+     * @see User.groupAdmin
+     * @see User.member
+     *
+     */
     constructor(type: UserType, groupId: Long?, userId: Long?) : this() {
         this.type = type
         this.groupId = groupId
