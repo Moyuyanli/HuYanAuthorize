98d85ad73feaa7a1f5983ca3ffcd4c90dd8ad9ce
Moyuyanli <RoxyGreerad@163.com>
2024-09-03 10:57:28 +0800

    release:1.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "cn.chahuyun"
-version = "1.1.2"
+version = "1.1.3"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/cn/chahuyun/authorize/HuYanAuthorize.kt
+++ src/main/kotlin/cn/chahuyun/authorize/HuYanAuthorize.kt
@@ -31,7 +31,7 @@ class HuYanAuthorize : KotlinPlugin(
         /**
          * 插件版本
          */
-        const val VERSION = "1.1.2"
+        const val VERSION = "1.1.3"
     }
 
     override fun onEnable() {
