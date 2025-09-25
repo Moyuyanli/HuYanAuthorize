64dd76c562c58ae50a9e49fc72e2de3905aa5288
moyuyanli <572490972@qq.com>
2025-09-25 08:52:18 +0800

    chore(build): 更新构建配置和依赖项
    - 将 Kotlin 版本从1.8.10 升级到 1.9.20
    - 移除 nexus-publish-plugin 插件- 添加 signing、java-library 和 maven-publish 插件
    - 启用 mavenCentral() 仓库并移除阿里云镜像
    - 移除旧的 Mirai 插件打包和文档生成任务
    - 注释掉手动发布的 Maven 配置和签名设置
    - 添加源码和 Javadoc 的 JAR 生成任务
    - 移除 BuildConfig 中的 BUILD_TIME 常量定义
    
    Signed-off-by: moyuyanli <572490972@qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 @file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
 
-import net.mamoe.mirai.console.gradle.wrapNameWithPlatform
-
+import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
-    val kotlinVersion = "1.8.10"
+    val kotlinVersion = "1.9.20"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
@@ -12,7 +11,12 @@ plugins {
 
     id("org.jetbrains.dokka") version "1.8.10"
     id("com.github.gmazzo.buildconfig") version "3.1.0"
-    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+
+
+    signing
+    `java-library`
+    `maven-publish`
+//    id("moe.karla.maven-publishing") version "1.3.1"
 }
 
 group = "cn.chahuyun"
@@ -20,9 +24,7 @@ version = "1.2.4"
 
 repositories {
     maven("https://nexus.jsdu.cn/repository/maven-public/")
-
-//    mavenCentral()
-//    maven("https://maven.aliyun.com/repository/public")
+    mavenCentral()
 }
 
 dependencies {
@@ -35,50 +37,6 @@ mirai {
     jvmTarget = JavaVersion.VERSION_11
 }
 
-nexusPublishing {
-    repositories {
-        create("sonatype") { // 对于2021年2月24日之后注册的用户
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username.set(project.findProperty("sonatypeUsername") as? String ?: System.getenv("SONATYPE_USERNAME"))
-            password.set(project.findProperty("sonatypePassword") as? String ?: System.getenv("SONATYPE_PASSWORD"))
-        }
-    }
-}
-
-tasks {
-    //打包mirai插件
-    create<net.mamoe.mirai.console.gradle.BuildMiraiPluginV2>("pluginJar") {
-        group = "mirai"
-        registerMetadataTask(
-            this@tasks,
-            "miraiPublicationPrepareMetadata".wrapNameWithPlatform(kotlin.target, true)
-        )
-        init(kotlin.target)
-        destinationDirectory.value(
-            project.layout.projectDirectory.dir(project.buildDir.name).dir("mirai")
-        )
-        archiveExtension.set("mirai2.jar")
-    }
-    //打包javadoc
-    register<Jar>("dokkaJavadocJar") {
-        group = "documentation"
-        dependsOn(dokkaJavadoc)
-        from(dokkaJavadoc.flatMap { it.outputDirectory })
-        archiveClassifier.set("javadoc")
-    }
-}
-
-//上传额外内容
-setupMavenCentralPublication {
-    artifact(tasks.kotlinSourcesJar)
-    artifact(tasks["pluginJar"])
-    artifact(tasks["dokkaJavadocJar"])
-}
-
-
-
-
 buildConfig {
     className("BuildConstants")
     packageName("cn.chahuyun.authorize")
@@ -90,3 +48,92 @@ buildConfig {
         "java.time.Instant.ofEpochSecond(${System.currentTimeMillis() / 1000L}L)"
     )
 }
+
+
+//  定义 Kotlin 源码 JAR
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(kotlin.sourceSets["main"].kotlin)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE // 跳过重复文件
+}
+
+// 定义 Dokka 生成的 Javadoc JAR
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.named<DokkaTask>("dokkaJavadoc"))
+}
+
+//mavenPublishing {
+//    // 设置成手动发布（运行结束后要到 Central 确认发布），如果要自动发布，就用 AUTOMATIC
+//    publishingType = PublishingType.USER_MANAGED
+//    // 改成你自己的信息
+//    url = "https://github.com/用户名/仓库"
+//    developer("用户名", "邮箱")
+//}
+
+
+// 配置发布
+//afterEvaluate {
+//    publishing {
+//        publications {
+//            create<MavenPublication>("mavenJava") {
+//                from(components["java"])
+//                // 手动指定主 JAR 文件
+//                artifact(tasks.named("buildPlugin"))
+//
+//                // 添加源码和文档
+//                artifact(sourcesJar)
+//                artifact(javadocJar)
+//
+//                // 手动配置 POM 元数据
+//                pom {
+//                    name.set("HuYan Authorize Plugin")
+//                    description.set("Mirai plugin for advanced authorization features")
+//                    url.set("https://github.com/Moyuyanli/HuYanAuthorize")
+//
+//                    licenses {
+//                        license {
+//                            name.set("MIT License")
+//                            url.set("https://opensource.org/licenses/MIT")
+//                        }
+//                    }
+//
+//                    developers {
+//                        developer {
+//                            id.set("moyuyanli")
+//                            name.set("Moyu yanli")
+//                            url.set("https://github.com/Moyuyanli")
+//                        }
+//                    }
+//
+//                    scm {
+//                        connection.set("scm:git:git://github.com/Moyuyanli/HuYanAuthorize.git")
+//                        developerConnection.set("scm:git:ssh://github.com/Moyuyanli/HuYanAuthorize.git")
+//                        url.set("https://github.com/Moyuyanli/HuYanAuthorize")
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+////    nexusPublishing {
+////        repositories {
+////            //旧仓库
+//////            sonatype()
+//////            //自定义仓库，用户和密码是xxxUsername和xxxPassword
+//////            create("central") {
+//////                nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+//////                snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+//////            }
+////            sonatype {
+////                nexusUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+////                snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+////            }
+////        }
+////    }
+//
+//    signing {
+//        useGpgCmd()
+////        sign(publishing.publications["mavenJava"])
+//    }
+//}
