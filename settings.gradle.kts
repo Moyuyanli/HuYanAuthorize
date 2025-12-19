4523c032a66b0528bc63a36bad74c6dad0331515
moyuyanli <572490972@qq.com>
2025-12-19 21:14:02 +0800

    build(gradle): 配置依赖解析管理并添加KSP处理器模块
    - 添加了 dependencyResolutionManagement 配置块
    - 设置仓库模式为 FAIL_ON_PROJECT_REPOS
    - 添加了自定义 Maven 仓库地址
    - 包含了必要的 Google 和 Maven Central 仓库
    - 添加了 ksp-processor 模块到项目中
    
    Signed-off-by: moyuyanli <572490972@qq.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven("https://nexus.chahuyun.cn/repository/maven-public/")
+        google()        // 必须包含这个
+        mavenCentral()  // 必须包含这个
+    }
+}
+
 rootProject.name = "HuYanAuthorize"
+
+include("ksp-processor")
